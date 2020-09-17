3dd91da3bd882f878e6dc654cf760795f8fea114
Kamil Antos <kml.antos@gmail.com>
2020-09-17 10:30:57 +0200

    Test script added

---
+++ Tests.m
@@ -0,0 +1,10 @@
+%% Example Title
+% Summary of example objective
+
+%% Section 1 Title
+% Description of first code block
+MF = MouseFinder()
+
+%% Section 2 Title
+% Description of second code block
+b = 2;
